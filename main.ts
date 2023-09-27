a6f99a1d51c69c0e99d3f03155f9d70f7191a8f7
Nataliia Odai <nataliiaodai22@gmail.com>
2023-09-27 19:10:00 +0300

    before mongoDb binding

--- main.ts
+++ main.ts
@@ -1,28 +1,42 @@
 import {TaskModel} from "./tasks/task.model";
 import {TaskService} from "./tasks/task.service";
 
+
 const taskService = new TaskService();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require("express")
 const app = express()
 
-app.use(cors());
-app.use(bodyParser.json());
-app.get("/",function(request,response){
-    response.send("Hello World!")
-})
 
+
+app.use(cors());
+app.use(bodyParser?.json());
+
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/todolist', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
 
 
 app.get('/tasks', (req, res) => {
     let tasksToReturn: TaskModel[] = taskService.getTasks()
-    // res.status(200).json({});
+
     if (!tasksToReturn) {
         res.status(404).json({error: 'No tasks found'});
     } else {
         res.status(200).json(tasksToReturn);
     }
+
 })
 
 
@@ -35,6 +49,7 @@ app.post('/tasks', (req, res) => {
         res.status(404).json({error: 'Invalid request'});
     }
 })
+
 
 
 app.put('/tasks/:taskId', (req, res) => {
@@ -66,3 +81,10 @@ app.delete('/tasks/:taskId', (req, res) => {
 app.listen(10000, function () {
     console.log("Started application on port %d", 10000)
 });
+
+
+
+
+
+
+
